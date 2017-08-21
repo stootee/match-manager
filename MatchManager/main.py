b0063cc3e2e0beea40057fd063f33b6080b12789
stuart.turner <stuart.turner@just-eat.com>
2017-08-21 01:34:49 +0100

    toggle buttons

--- MatchManager/main.py
+++ MatchManager/main.py
@@ -7,11 +7,12 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
-from kivy.properties import StringProperty, NumericProperty, ObjectProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty, ListProperty
 from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.behaviors import ToggleButtonBehavior
 from datetime import datetime
 import time
 import logging
@@ -29,12 +30,54 @@ TODO:
 class MainScreenManager(ScreenManager):
     _elapsedtime = NumericProperty(0)
     timestr = StringProperty()
+    _minutes = NumericProperty()
     _running = False
     _update = True
+    _goals = []
+    _home = NumericProperty(0)
+    _away = NumericProperty(0)
+    _h_scorers = ListProperty([])
+    _a_scorers = ListProperty([])
 
     def __init__(self, **kwargs):
         super(MainScreenManager, self).__init__(**kwargs)
         self.increment_time(0)
+
+    def record_who_scored(self):
+        t = ToggleButtonBehavior.get_widgets('team')
+        s = ToggleButtonBehavior.get_widgets('scorer')
+        _team_who_scored = None
+        _tws_name = None
+        _person_who_scored = None
+        minutes = self._minutes
+
+        for _t in t:
+            if _t.state == 'down':
+                _team_who_scored = _t.text
+                _tws_name = _t.name
+                if _t.name == 'home':
+                    self._home += 1
+                else:
+                    self._away += 1
+        for _s in s:
+            if _s.state == 'down':
+                _person_who_scored = _s.text
+                if _tws_name == 'home':
+                    self._h_scorers.append('%s (%s)' % (_s.text, minutes))
+                else:
+                    self._a_scorers.append('%s (%s)' % (_s.text, minutes))
+
+        if _team_who_scored and _person_who_scored:
+            self._goals.append((_team_who_scored, _person_who_scored, self.timestr),)
+            self.current = 'manager_screen'
+            self._update = True
+            print self._goals
+        else:
+            popup = Popup(title='Missing Info',
+                          content=Label(text='!'),
+                          size_hint=(None, None), size=(self.width * 0.5, self.height * 0.5)
+                          )
+            popup.open()
 
     def _logger(self, log_details):
         print(run_uid, datetime.now().strftime('%Y%m%d %H:%M:%S'), log_details)
@@ -50,6 +93,8 @@ class MainScreenManager(ScreenManager):
                 self.timestr = '%02d:%02d:%02d' % (hours, minutes, seconds)
             else:
                 self.timestr = '%02d:%02d.%02d' % (minutes, seconds, hseconds)
+        else:
+            self._minutes = int(elap / 60) + 1
         return '%02d:%02d:%02d.%02d' % (hours, minutes, seconds, hseconds)
 
     def increment_time(self, interval):
@@ -86,7 +131,6 @@ class GoalScreen(Screen):
     def __init__(self, **kwargs):
         super(GoalScreen, self).__init__(**kwargs)
 
-
 class CPopup(Popup):
     def __init__(self, **kwargs):
         super(CPopup, self).__init__(**kwargs)
@@ -112,7 +156,7 @@ class MatchManagerApp(App):
         super(MatchManagerApp, self).__init__(**kwargs)
 
     def build(self):
-        Window.size = (300, 300)
+        Window.size = (300, 500)
         return root_widget
 
 if __name__ == '__main__':
